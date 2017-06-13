1724a5095a2478137b345f6e8d28bcb973fbbaf8
Lisa Schoofs <lisaschoofs@gmail.com>
2017-06-12 20:01:17 -0500

    added ArrayList, printed with for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,22 @@
+import java.util.*;
+
 public class Hello {
 
   public static void main(String[] args) {
 
-    string helloWorld = "Hello World!"
-    System.out.println( helloWorld );
+    System.out.println( "Hello World!" );
+
+    ArrayList<String> greetings = new
+    ArrayList<String>();
+
+    greetings.add("Hola");
+    greetings.add("Bonjour");
+    greetings.add("Ni hao");
+    greetings.add("Jambo");
+
+    for (String hello: greetings) {
+      System.out.println(hello);
+    }
 
   }
 }
